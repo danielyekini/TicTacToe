0d79c31920888b55cdbeee18de36cc5aa1ffec6b
danielyekini <130864683+danielyekini@users.noreply.github.com>
2023-04-26 01:49:53 +0100

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        GameControl.start();
+        // CPUPerfect.play('X');
+        // int userPos = Input.integer("Select Position (1-9): ");
+        // Grid.placePosition(userPos, 'O');
+        //CPUPerfect.play('X');
+
+    }
+}
